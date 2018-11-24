3e1cdaa7fe9b88c6c400723b7e9b21b3419c1b5d
Casper Vinther <caspervin@caspers-imac.local>
2018-11-24 13:36:25 +0100

    med login

---
+++ PNRustfri 1.0/ViewControllerLog1.swift
@@ -0,0 +1,30 @@
+//
+//  ViewControllerLog1.swift
+//  PNRustfri 1.0
+//
+//  Created by Casper Vinther on 18/11/2018.
+//  Copyright © 2018 Casper Vinther. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerLog1: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Podfile
@@ -0,0 +1,27 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'PNRustfri 1.0' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for PNRustfri 1.0
+
+    pod 'Firebase/Core'
+    pod 'Firebase/Auth'
+
+  target 'PNRustfri 1.0' do
+	pod 'JTAppleCalendar', '~> 7.0'
+	end	
+
+  target 'PNRustfri 1.0Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'PNRustfri 1.0UITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
