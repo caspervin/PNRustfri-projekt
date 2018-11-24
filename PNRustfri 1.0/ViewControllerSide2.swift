244dd8568672a2d1a947777649ee941a76cd8362
Casper <casper-kage@hotmail.com>
2018-11-24 14:19:01 +0100

    s

---
+++ PNRustfri 1.0/ViewControllerSide2.swift
@@ -0,0 +1,42 @@
+//
+//  ViewControllerSide2.swift
+//  PNRustfri 1.0
+//
+//  Created by Casper Vinther on 16/11/2018.
+//  Copyright © 2018 Casper Vinther. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class ViewControllerSide2: UIViewController {
+
+    @IBOutlet weak var emailTextfield: UITextField!
+    
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func LoginTabbed(_ sender: Any) {
+         if let email = self.emailTextfield.text, let password = self.passwordTextfield.text {
+            
+            Auth.auth().signIn(withEmail: email, password: password) { (user , error) in
+                if user != nil {
+                    print ("Succes!")
+                    
+                }
+                if error != nil {
+                    print ("ups")
+                }
+
+}
+}
+}
+
+}
